76187c601dc9f5e3c7413983281e26e12be2e8e7
Rhys Reene <2016201@folkestonegirls.kent.sch.uk>
2021-12-02 12:13:51 +0000

    Day 1 Part 1

--- PuzzleSolutions/Y2021/Y2021-D1.cs
+++ PuzzleSolutions/Y2021/Y2021-D1.cs
@@ -10,6 +10,28 @@ namespace PuzzleSolutions.Y2021
         public static string SolvePart1()
         {
             List<int> puzzleInput = MyIO.ReadIntsFromFile(2021, 1, 1);
+            int numberOfIncreases = 0;
+            int previousReading = 0;
+            foreach (int reading in puzzleInput)
+            {
+
+                if (reading > previousReading)
+                {
+                    numberOfIncreases = numberOfIncreases + 1;
+
+                    Console.WriteLine($"{reading} = (increased)");
+                }
+                else if (reading < previousReading)
+                {
+                    Console.WriteLine($"{reading} = (decreased)");
+                }
+                else
+                {
+                    Console.WriteLine($"{reading} = (equal)");
+                }
+                previousReading = reading;
+            }
+            Console.WriteLine($"Number of increases = {numberOfIncreases - 1}"); //minus one to account for the first number not being an increase or decrease
             return "I Haven't Solved part 1 yet!";
         }
 
